5b53a1cda34221042b427305b321cfebd090bbc6
李金钊 <jinzhao.li@lanmaoly.com>
2018-04-09 15:03:16 +0800

    ThreadLocal补充

--- src/main/java/com/jzli/async/concurrent/ThreadLocalTest.java
+++ src/main/java/com/jzli/async/concurrent/ThreadLocalTest.java
@@ -10,6 +10,8 @@ import java.util.concurrent.TimeUnit;
  * @Author ：李金钊
  * @Version ：0.0.1
  * @Description ：ThreadLocal使用ThreadLocalMap存储数据，每个线程都有ThreadLocalMap的引用，以ThreadLocal作为key，以实际值作为value。
+ *                ThreadLocal真正存放数据的地方，就是线程对象本身。
+ *                一个线程可以被多个ThreadLocal关联，每声明一个，就在线程的threadLocals增加为一个键值对，key为 ThreadLocal,而value为具体存放的对象。
  * ========================================================
  */
 public class ThreadLocalTest {
