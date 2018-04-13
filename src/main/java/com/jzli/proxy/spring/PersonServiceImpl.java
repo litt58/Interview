3c29ff39a9e2a374c8f11b1f2a025a2dacb1214e
李金钊 <jinzhao.li@lanmaoly.com>
2018-04-13 17:01:45 +0800

    补充Spring AOP动态代理时，使用注入的Spring代理对象的方式，调用类的方法

--- src/main/java/com/jzli/proxy/spring/PersonServiceImpl.java
+++ src/main/java/com/jzli/proxy/spring/PersonServiceImpl.java
@@ -1,19 +1,23 @@
 package com.jzli.proxy.spring;
 
-import org.springframework.aop.framework.AopContext;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 //编写实现类
 @Service
 public class PersonServiceImpl implements IPersonService {
+    @Autowired
+    private IPersonService service;
 
     public String action(String msg) {
         System.out.println("FooService, method doing.");
         //直接调用方法，无法使用AOP切入
-//        this.work(msg);
+        this.work(msg);
 
         //使用代理类，调用方法
-        ((IPersonService) AopContext.currentProxy()).work(msg);
+//        ((IPersonService) AopContext.currentProxy()).work(msg);
+        //使用注入的Spring代理对象
+        service.work(msg);
         return "[" + msg + "]";
     }
 
