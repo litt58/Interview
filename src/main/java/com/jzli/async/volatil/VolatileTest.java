a4b5584ae1a977bfb0feeeebe9477de72fe421a3
李金钊 <jinzhao.li@lanmaoly.com>
2018-04-17 17:22:37 +0800

    增加volatile

---
+++ src/main/java/com/jzli/async/volatil/VolatileTest.java
@@ -0,0 +1,44 @@
+package com.jzli.async.volatil;
+
+/**
+ * =======================================================
+ *
+ * @Company 产品技术部
+ * @Date ：2018/4/17
+ * @Author ：李金钊
+ * @Version ：0.0.1
+ * @Description ：volatile增加了实例变量在对线程之间的可见性，保证我们获得的是变量的最新值。
+                  volatile在读上面保持了同步作用，但是在写上面不保持同步
+ * ========================================================
+ */
+public class VolatileTest {
+
+    public static void main(String[] args) throws InterruptedException {
+        RunThread runThread = new RunThread();
+        runThread.start();
+        Thread.sleep(1000);
+        runThread.setRunning(false);
+    }
+}
+
+class RunThread extends Thread {
+    private volatile boolean isRunning = true;
+
+
+    public boolean isRunning() {
+        return isRunning;
+    }
+
+    public void setRunning(boolean running) {
+        isRunning = running;
+    }
+
+    @Override
+    public void run() {
+        System.out.println(Thread.currentThread().getName() + "开始运行！");
+        while (isRunning) {
+
+        }
+        System.out.println(Thread.currentThread().getName() + "被中止了！");
+    }
+}
