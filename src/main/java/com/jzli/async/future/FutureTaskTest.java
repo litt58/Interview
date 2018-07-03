ddbec884e574b83f012f9390ea3206600589824c
李金钊 <jinzhao.li@lanmaoly.com>
2018-07-03 15:59:58 +0800

    Future

---
+++ src/main/java/com/jzli/async/future/FutureTaskTest.java
@@ -0,0 +1,35 @@
+package com.jzli.async.future;
+
+import java.util.Date;
+import java.util.concurrent.*;
+
+/**
+ * =======================================================
+ *
+ * @Company 技术中心-共享服务部-后端服务部
+ * @Date ：2018/7/3
+ * @Author ：李金钊
+ * @Version ：0.0.1
+ * @Description ：
+ * ========================================================
+ */
+public class FutureTaskTest {
+    public static void main(String[] args) throws ExecutionException, InterruptedException, TimeoutException {
+        Callable<String> callable = () -> {
+            String name = Thread.currentThread().getName();
+            String s = new Date().toLocaleString();
+            TimeUnit.SECONDS.sleep(5);
+            return name + "\t" + s;
+        };
+
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+       Future<String> submit1 = executorService.submit(callable);
+        Future<?> submit2 = executorService.submit(new FutureTask<String>(callable));
+        executorService.shutdown();
+        String s1=submit1.get();
+        System.out.println(s1);
+        String s2 = (String) submit2.get(10, TimeUnit.SECONDS);
+        System.out.println(s2);
+
+    }
+}
